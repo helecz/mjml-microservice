837539ef204ea45178d079f2e3bd4b8abf0a392b
Josef Kříž <pepakriz@gmail.com>
2016-06-15 15:13:37 +0200

    Update babel version, add default ignore

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-require('babel-register');
+require('babel-register')({
+	ignore: function(filename) {
+		if (/hele-mjml-components/.test(filename)) {
+			return false;
+		}
+		return /node_modules/.test(filename);
+	},
+});
 
 require('./main');
