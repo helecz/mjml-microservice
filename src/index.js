21c72b96dd33c3281567d72f4d856377055bc89d
Josef Kříž <pepakriz@gmail.com>
2016-07-13 10:16:04 +0200

    Update eslint & mjml

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 require('babel-register')({
-	ignore: function(filename) {
-		if (/hele-mjml-components/.test(filename)) {
-			return false;
-		}
-		return /node_modules/.test(filename);
-	},
+  ignore: function (filename) {  // eslint-disable-line object-shorthand, func-names
+    if (/hele-mjml-components/.test(filename)) {
+      return false;
+    }
+    return /node_modules/.test(filename);
+  },
 });
 
 require('./main');
--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import {mjml2html} from 'mjml';
+import { mjml2html } from 'mjml';
 import config from './config';
 
 const app = express();
 
 app.use(bodyParser.raw({
-  type: () => true
+  type: () => true,
 }));
 
 app.get('/', (req, res) => {
@@ -17,12 +17,11 @@ app.post('/compile', (req, res) => {
   try {
     const responseData = mjml2html(`${req.body}`);
     res.send(responseData);
-
   } catch (e) {
     res.status(422).send('Invalid input');
   }
 });
 
 app.listen(config.port, () => {
-  console.log('Server started at port %s', config.port);
+  console.log('Server started at port %s', config.port); // eslint-disable-line no-console
 });
